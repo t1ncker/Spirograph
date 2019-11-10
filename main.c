965c8250fbe588d90c93f4409846bdd1ac6b3b27
t1ncker <tincker@yandex.ru>
2019-11-10 17:25:45 +0300

    added interrupt setup, changed lazer variables names

--- main.c
+++ main.c
@@ -10,11 +10,11 @@ Mega 328PB
 
 #define F_CPU 8000000
 
-volatile char lazer_stat = 0;
+volatile char lazer_cntl = 0;
 
 ISR(INT0_vect) //interrupt 0 handler
 {
-	(lazer_stat == 0)?(lazer_stat = 1):(lazer_stat = 0);
+	(lazer_cntl == 0)?(lazer_cntl = 1):(lazer_cntl = 0);
 }
 
 void preset(void)
@@ -29,8 +29,10 @@ void preset(void)
 	ADMUX |= (0<<REFS1)|(1<<REFS0) 		//AVcc with external capacitor at AREF pin
 	|(0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(0<<MUX0); //Default ADC channel 0
 
-
-	sei();		//enable global interrupts
+	//Interrupt setup
+	GICR |= (1<<INT0); 		 //INT0 interrupt enable
+	MCUCR &= ~(1<<ISC01)|(1<<ISC00); //Interrupt on rising edge on pin PD2
+	sei();				 //enable global interrupts
 
 	Lcd_init();	//enable lcd screen
 	Lcd_clear();
@@ -39,7 +41,7 @@ void preset(void)
 int readADC ()
 {
 	ADCSRA |= (1<<ADSC); //Start conversion
-	while(ADCSRA & (1<<ADSC));
+	while(!(ADCSRA & (1<<ADIF)));//Waiting conversion to finish
 	return ADC;
 }
 
@@ -47,19 +49,19 @@ int main(void)
 {
 	preset();
 
-	char laz_onoff[4];
+	char lazer_stat[4];
 
 	while(1){
 		Lcd_clear();
 		if(lazer_stat == 1){
 			PORTD |= (1<<PD4);
-			laz_onoff = "ON";
+			lazer_stat = "ON";
 		}else{
 			PORTD |= (0<<PD4);
-			laz_onoff = "OFF";
+			lazer_stat = "OFF";
 		}
 		Lcd_prints(1, 1, FONT_1X, (unsigned char*)"Lazer:");
-		Lcd_print(8, 1, FONT_1X, (unsigned char*) laz_onoff);
+		Lcd_print(8, 1, FONT_1X, (unsigned char*) lazer_stat);
 
 	}
 }
